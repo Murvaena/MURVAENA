f862af5477bfe0426de4e2be8b71f5eebd5ceda0
murvaena <99964711+Murvaena@users.noreply.github.com>
2023-10-09 20:47:26 +0800

    Add files via upload
    day 1

---
+++ tipe_data_dan_variabel.java
@@ -0,0 +1,22 @@
+public class  tipe_data_dan_variabel {
+
+    public static void main(String[] args) {
+      //membuat varuabel
+      String nama, alamat;
+      int usia;
+      double tinggi;
+      
+      //mengisi variabel diatas
+      nama = "murvaena";
+        System.out.println("nama: "+nama );
+      alamat = "balombong";
+      System.out.println("alamat: "+alamat );
+      tinggi = 148;
+      System.out.println("tinggi: "+ tinggi + "cm");
+       usia = 20;
+      System.out.println("usia: "+usia);
+      
+    }
+    
+}
+
